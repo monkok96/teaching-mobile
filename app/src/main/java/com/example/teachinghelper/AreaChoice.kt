35f061629db36804fafcb11883e6c638b81286f8
DESKTOP-ASA2QOQ\Monia <monikakokot96@gmail.com>
2020-01-12 19:12:14 +0100

    code cleanup, init navigation to questions

--- app/src/main/java/com/example/teachinghelper/AreaChoice.kt
+++ app/src/main/java/com/example/teachinghelper/AreaChoice.kt
@@ -1,5 +1,6 @@
 package com.example.teachinghelper
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,30 +9,34 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.teachinghelper.ViewModels.AreasViewModel
 import android.widget.Button
 import android.widget.TextView
+import com.example.teachinghelper.Entities.Area
 import com.example.teachinghelper.ViewModels.SubjectViewModel
 
 
 class AreaChoice : AppCompatActivity() {
     private lateinit var areasViewModel: AreasViewModel
     private lateinit var subjectViewModel: SubjectViewModel
+    private var subjectId: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_area_choice)
 
-        val subjectId = intent.getIntExtra("subjectId", 1)
+        subjectId = intent.getIntExtra("subjectId", 1)
+        setSubjectName()
+        setButtons()
+    }
+
+    private fun setSubjectName() {
         subjectViewModel = ViewModelProviders.of(this).get(SubjectViewModel::class.java)
         val subjectName =  subjectViewModel.getSubjectById(subjectId).name
-
-
-
         var subjectNameText = findViewById<TextView>(R.id.subjectName)
         subjectNameText.text = subjectName
+    }
 
-        areasViewModel = ViewModelProviders.of(this).get(AreasViewModel::class.java)
 
-        var areas = areasViewModel.areasBySubject(subjectId)
-
-        var buttonsLayout = findViewById(R.id.areaButtonsLayout) as LinearLayout;
+    private fun setButtons() {
+        var buttonsLayout = findViewById<LinearLayout>(R.id.areaButtonsLayout)
 
         var layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
@@ -39,6 +44,7 @@ class AreaChoice : AppCompatActivity() {
         )
         layoutParams.setMargins(0, 50, 0, 0)
 
+        var areas = getAreas()
         for (area in areas) {
             val btnTag = Button(this).also {
                 it.layoutParams = layoutParams
@@ -48,7 +54,18 @@ class AreaChoice : AppCompatActivity() {
                 it.setTextColor(Color.parseColor("#ffffff"))
             }
             buttonsLayout.addView(btnTag)
-//            (findViewById(area.id) as Button).setOnClickListener(this)
+
+            btnTag.setOnClickListener {
+                //TODO: pass questions activity class here
+//                val intent = Intent(this, Questions::class.java)
+//                intent.putExtra("areaId", area.id)
+//                startActivity(intent)
+            }
         }
+    }
+
+    private fun getAreas(): List<Area> {
+        areasViewModel = ViewModelProviders.of(this).get(AreasViewModel::class.java)
+        return areasViewModel.areasBySubject(subjectId)
     }
 }
